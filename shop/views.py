1eb3308eaafaae5d0664d976e3cdf18a2a269263
Alderaan <65733015+Sokol29102@users.noreply.github.com>
2025-12-16 02:58:41 +0200

    update
    тепер воно фактично не дозволяє незалогіненому навіть спробувати запостити замовлення

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Drone
 from .forms import OrderForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.views import redirect_to_login
+
+
 
 def home(request):
     return render(request, 'shop/index.html')
@@ -15,22 +20,20 @@ def product(request):
 
 
 def detail_drone(request, pk):
-    drone = Drone.objects.get(id=pk)
+    drone = get_object_or_404(Drone, id=pk)
 
     if request.method == "POST":
-        form = OrderForm(request.POST)
+        if not request.user.is_authenticated:
+            return redirect_to_login(request.get_full_path())
 
+        form = OrderForm(request.POST)
         if form.is_valid():
             order = form.save(commit=False)
             order.client = request.user
             order.drone = drone
             order.save()
-            return redirect('home')
+            return redirect("home")
     else:
         form = OrderForm()
 
-    context = {
-        'drone': drone,
-        'form': form
-    }
-    return render(request, 'shop/detail.html', context)
+    return render(request, "shop/detail.html", {"drone": drone, "form": form})
